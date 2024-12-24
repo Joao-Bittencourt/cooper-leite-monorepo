67b60c8e30311abde2cbd4a8a86dabfd07e83e42
joao bittencourt <joaogsbittencourt@gmail.com>
2024-12-23 23:59:38 -0300

    em desenv

--- front-end/src/services/variables.js
+++ front-end/src/services/variables.js
@@ -1,5 +1,5 @@
-const API_URL = 'http://cpl-l.x10.mx/api'
-//const API_URL = 'http://localhost:8000/api'
+// const API_URL = 'http://cpl-l.x10.mx/api'
+const API_URL = 'http://localhost:8000/api'
 
 const apiUris = {
   login: '/login',
